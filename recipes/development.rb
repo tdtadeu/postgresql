b825409f8690ed68289c1e793c8bb7eb81cbbe6f
Thiago Dias <tdtadeu@gmail.com>
2016-01-14 19:04:33 -0200

    Enabling pg_trgm and unaccent

--- recipes/development.rb
+++ recipes/development.rb
@@ -1,2 +1,8 @@
 package "libpq-dev"
 package "postgresql-contrib"
+
+bash "Enable pg_trgm for PostgreSQL" do
+  code <<-EOH
+    sudo -u postgres psql -d prototype -c "create extension pg_trgm; create extension unaccent;"
+  EOH
+end
