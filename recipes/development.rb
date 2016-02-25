b397d1e6135c706448a11251f0bedae3ca53ee6c
Thiago Dias <tdtadeu@gmail.com>
2016-02-25 15:42:33 -0300

    Removing application dependency

--- recipes/development.rb
+++ recipes/development.rb
@@ -1,8 +1,2 @@
 package "libpq-dev"
 package "postgresql-contrib"
-
-bash "Enable pg_trgm for PostgreSQL" do
-  code <<-EOH
-    sudo -u postgres psql -d prototype -c "create extension pg_trgm; create extension unaccent;"
-  EOH
-end
